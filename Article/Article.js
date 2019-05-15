79e8f0db3f6f6a41e1e80825e4ec247bbd61bfbc
Anna W <anski.anna@gmail.com>
2019-05-15 13:33:18 +0100

    menu functionality to drop-dwon now works, and the articles have expand buttons

--- Article/Article.js
+++ Article/Article.js
@@ -3,18 +3,20 @@
 class Article {
   constructor(domElement) {
     // assign this.domElement to the passed in domElement
-    this.domElement;
+    this.domElement = domElement;
+    // console.log(domElement)
     // create a reference to the ".expandButton" class. 
-    this.expandButton;
+    const expandButton = this.domElement.querySelector('.expandButton');
     // Using your expandButton reference, update the text on your expandButton to say "expand"
-    
+    expandButton.textContent = "expand";
     // Set a click handler on the expandButton reference, calling the expandArticle method.
-    
+    // this.expandArticle = this.expandArticle.bind(this);
+    expandButton.addEventListener('click', this.expandArticle.bind(this));
   }
 
-  expandArticle() {
+  expandArticle(event) {
     // Using our reference to the domElement, toggle a class to expand or hide the article.
-
+    this.domElement.classList.toggle('article-open');
   }
 }
 
@@ -26,4 +28,6 @@ class Article {
 
 */
 
-let articles;
+let articles = document.querySelectorAll('.article');
+
+articles.forEach(article => new Article(article));
